7e429bf3b5980539cdf3757f037bbd09c1f8b8c3
hqtnpb <binh141023@gmail.com>
2025-03-04 11:55:47 +0700

    Destination-page(Done)

--- frontend/src/components/Layout/HeaderOnly/index.js
+++ frontend/src/components/Layout/HeaderOnly/index.js
@@ -2,10 +2,8 @@ import Header from "~/components/Layout/components/Header/Header";
 function HeaderOnly({ children }) {
     return (
         <div>
-            <Header />
-            <div className="container">
-                <div className="content">{children}</div>
-            </div>
+            <Header /> 
+            <div className="content">{children}</div>
         </div>
     );
 }
